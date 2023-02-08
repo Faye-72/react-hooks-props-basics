7c77af0310ee899c7facf19e7fcfece5f6191d7b
fmitei <faith.mitei@trustgro.com>
2023-02-08 05:47:06 +0300

    props basic follow through

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  return (
+    <div id="blog-content">
+      {props.articleText}
+      console.log(props)
+    </div>
+  );
 }
 
 export default BlogContent;
